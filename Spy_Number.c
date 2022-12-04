15d87fef4ddfbddfeb4332863ffc72c49ddf9423
omteja04 <119728012+omteja04@users.noreply.github.com>
2022-12-04 19:05:45 +0530

    #include<stdio.h> int main() {     int n,d,p=1,s=0;     scanf("%d",&n);     while(n>0)     {     d=n%10;     n=n/10;     s=s+d;     p=p*d;     }     if(s==p)     printf("Spy Number");     else     printf("Not Spy Number");
    }

---
+++ Spy_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,p=1,s=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+    d=n%10;
+    n=n/10;
+    s=s+d;
+    p=p*d;
+    }
+    if(s==p)
+    printf("Spy Number");
+    else
+    printf("Not Spy Number");
+    
+}
