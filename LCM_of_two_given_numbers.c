b9f9305489a972fdecf891e1a9bfd6e757d57ee6
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2022-12-05 18:55:21 +0530

    #include<stdio.h> int main() {     int a,b,l;     scanf("%d %d",&a,&b);     l=(a>b)?b:a;     while(1)     {     if(l%a==0 && l%b==0)     {     printf("%d",l);     break;     }     l++;     } }

---
+++ LCM_of_two_given_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,l;
+    scanf("%d %d",&a,&b);
+    l=(a>b)?b:a;
+    while(1)
+    {
+    if(l%a==0 && l%b==0)
+    {
+    printf("%d",l);
+    break;
+    }
+    l++;
+    }
+}
