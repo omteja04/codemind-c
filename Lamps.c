43bf6333857f36a41616e0b9aac88d5985c9e295
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2022-12-16 21:27:27 +0530

    #include<stdio.h> int main() {     int n,k,x,y;     scanf("%d %d %d %d",&n,&k,&x,&y);     int rc;//redcost     rc=x*k;     if(x<y)     printf("%d",rc+x*(n-k));     else     printf("%d",rc+y*(n-k)); }

---
+++ Lamps.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,x,y;
+    scanf("%d %d %d %d",&n,&k,&x,&y);
+    int rc;//redcost
+    rc=x*k;
+    if(x<y)
+    printf("%d",rc+x*(n-k));
+    else
+    printf("%d",rc+y*(n-k));
+}
