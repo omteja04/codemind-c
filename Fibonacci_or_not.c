c5c08b4c4b39afed99c3e3d321eb2c2ea291e8c4
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2022-12-20 18:33:28 +0530

    #include<stdio.h> int main() {     int n,i=0,j=1,c,k,flag=0;     scanf("%d",&n);     for(c=3;c<=n;c++)     {     k=i+j;     i=j;     j=k;     if(k==n)     flag=1;     }     if(flag==1)     printf("True");     else     printf("False");
    }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0,j=1,c,k,flag=0;
+    scanf("%d",&n);
+    for(c=3;c<=n;c++)
+    {
+    k=i+j;
+    i=j;
+    j=k;
+    if(k==n)
+    flag=1;
+    }
+    if(flag==1)
+    printf("True");
+    else
+    printf("False");
+    
+}
