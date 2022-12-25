88a57145d3d11639bc099a0fb7339e308d5f9a8b
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2022-12-25 11:52:49 +0530

    #include<stdio.h> int main() {     int n,i,j,sum=0;     scanf("%d",&n);     while(sum !=1 && sum!=4)     {         sum=0;         while(n)         {             j=n%10;             sum+= j*j;             n/=10;         }         n=sum;     }     if(sum==1 || sum==7)     printf("True");     else     printf("False"); }

---
+++ happy_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,sum=0;
+    scanf("%d",&n);
+    while(sum !=1 && sum!=4)
+    {
+        sum=0;
+        while(n)
+        {
+            j=n%10;
+            sum+= j*j;
+            n/=10;
+        }
+        n=sum;
+    }
+    if(sum==1 || sum==7)
+    printf("True");
+    else
+    printf("False");
+}
