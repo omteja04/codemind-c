343c7cb345a47ec415795b577a78f45620d5519b
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-03 22:29:10 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     printf("%.2f",2.54*a); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,13 +1,7 @@
 #include<stdio.h>
-void itom(int a)
-{
-    float cm;
-    cm=2.54*a;
-    printf("%.2f",cm);
-}
 int main()
 {
     int a;
     scanf("%d",&a);
-    itom(a);
+    printf("%.2f",2.54*a);
 }
