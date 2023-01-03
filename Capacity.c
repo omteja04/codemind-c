608c8fc13e577d1627af9f15b14ad6bf869a035d
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-03 22:30:32 +0530

    #include<stdio.h> void capacity(int s,int t,int b) {     printf("%d KB",s*t*b); } int main() {     int s,t,b;     scanf("%d%d%d",&s,&t,&b);     capacity(s,t,b);
    }

--- Capacity.c
+++ Capacity.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 void capacity(int s,int t,int b)
 {
-    printf("%dkb",s*t*b);
+    printf("%d KB",s*t*b);
 }
 int main()
 {
     int s,t,b;
     scanf("%d%d%d",&s,&t,&b);
     capacity(s,t,b);
+    
 }
