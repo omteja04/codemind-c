5f0273ae6e2dfee9218de6f5452552ecf70ca8d6
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-04 23:00:11 +0530

    #include<stdio.h> int main() {     int b1,b2,b3,avg;     scanf("%d%d%d",&avg,&b1,&b2);     b3=3*avg-b1-b2;     printf("%d",b3); }

--- Average_weight.c
+++ Average_weight.c
@@ -1,15 +1,8 @@
 #include<stdio.h>
-#include<math.h>
-void weight(int x,int w1,int w2)
-{
-    int w3;
-    w3=3*x-(w1+w2);
-    printf("%d",w3);
-}
 int main()
 {
-    int x,w1,w2;
-    scanf("%d%d%d",&x,&w1,&w2);
-    weight(x,w1,w2);
-    
+    int b1,b2,b3,avg;
+    scanf("%d%d%d",&avg,&b1,&b2);
+    b3=3*avg-b1-b2;
+    printf("%d",b3);
 }
