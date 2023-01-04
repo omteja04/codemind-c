5aa7bec84f6ef3733d4bd5167e882b862a7e0662
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-04 23:02:38 +0530

    #include<stdio.h> int main() {     int d,y,r,w;     scanf("%d",&d);     y=d/365;     r=d%365;     w=r/7;     printf("%d %d",y,w);
    }

--- Days_into_years,_weeks:.c
+++ Days_into_years,_weeks:.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
-void yrweek(int d)
-{
-    int y,w;
-y=d/365;
-d=d%365;
-w=d/7;
-
-    printf("%d
-%d",y,w);
-}
 int main()
 {
-    int y,d,w;
+    int d,y,r,w;
     scanf("%d",&d);
-    yrweek(d);
+    y=d/365;
+    r=d%365;
+    w=r/7;
+    printf("%d
+%d",y,w);
+    
 }
