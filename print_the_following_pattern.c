ca6c2c05381c9c9c5ec6a30c26d837e9d53f7efd
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-05 14:12:27 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         printf("%d",j);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,8 +5,8 @@ int main()
     scanf("%d",&n);
     for(i=n;i>=1;i--)
     {
-        for(j=n;j>=1;j--)
-        printf("%d ",j);
+        for(j=1;j<=i;j++)
+        printf("%d",j);
         printf("
 ");
     }
