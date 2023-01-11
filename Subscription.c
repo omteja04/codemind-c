a552dfcafb1f6b13b7cb9b6cd8c22a4b4cb182bc
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-11 22:04:00 +0530

    #include<stdio.h> int main() {     int tc,k;     scanf("%d",&tc);     for(k=1;k<=tc;k++)     {         int x;         scanf("%d",&x);         if(x>30)         printf("YES ");         else         printf("NO ");     }
    }

---
+++ Subscription.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int tc,k;
+    scanf("%d",&tc);
+    for(k=1;k<=tc;k++)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x>30)
+        printf("YES
+");
+        else
+        printf("NO
+");
+    }
+    
+}
