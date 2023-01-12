544796318e0776a5bba4c8d639b4b590977341fa
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-12 09:25:28 +0530

    #include<stdio.h> int main() {     int n1,d1,sum1=0,sq1,n2,d2,sum2=0,sq3,sq2;     scanf("%d",&n1),     sq1=n1*n1;     while(n1)     {         d1=n1%10;         n1=n1/10;         sum1=sum1*10+d1;     }     n2=sum1;     sq2=n2*n2;     while(sq2)     {       d2=sq2%10;       sq2=sq2/10;       sum2=sum2*10+d2;     }     sq3=sum2;     if(sq3==sq1)     printf("True");     else     printf("False");     return 0; }

--- Adam_number.c
+++ Adam_number.c
@@ -1,29 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    int k,i,d,s=0;
-    k=n*n;
-    while(n>0)
+    int n1,d1,sum1=0,sq1,n2,d2,sum2=0,sq3,sq2;
+    scanf("%d",&n1),
+    sq1=n1*n1;
+    while(n1)
     {
-        d=n%10;
-        s=s*10+d;
-        n=n/10;
+        d1=n1%10;
+        n1=n1/10;
+        sum1=sum1*10+d1;
     }
-    int b,c,rev=0,f;
-    
-    b=s*s;
-    while(b>0)
+    n2=sum1;
+    sq2=n2*n2;
+    while(sq2)
     {
-        f=b%10;
-        rev=rev*10+f;
-        b=b/10;
+      d2=sq2%10;
+      sq2=sq2/10;
+      sum2=sum2*10+d2;
     }
-
-    if(rev==k)
+    sq3=sum2;
+    if(sq3==sq1)
     printf("True");
     else
     printf("False");
-    
+    return 0;
 }
