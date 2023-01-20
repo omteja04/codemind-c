3b1a38214e969c4d7fb8f4eb081feff043305fd2
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-20 17:45:45 +0530

    #include<stdio.h> int main() {     int n,i,s=1;     scanf("%d",&n);     for(i=2;i<n;i++)     if(n%i==0)     s+=i;     if(s==n)     printf("True");     else     printf("False"); }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,i,sum=0,old;
+    int n,i,s=1;
     scanf("%d",&n);
-   for(i=1;i<n;i++)
-   {
-      if(n%i==0)
-      sum=sum+i;
-   }
-    if(n==sum)
+    for(i=2;i<n;i++)
+    if(n%i==0)
+    s+=i;
+    if(s==n)
     printf("True");
     else
     printf("False");
-    
 }
