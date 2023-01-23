1c54568c0bf8ac3a3161704c20a71980b66179ee
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-23 22:07:48 +0530

    #include<stdio.h> int main() {     int n,i,f=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         f+=i;     }     if(f>n)     printf("True");     else     printf("False"); }

--- Abundant_Numbers.c
+++ Abundant_Numbers.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,f=0,i;
+    int n,i,f=0;
     scanf("%d",&n);
     for(i=1;i<n;i++)
-    if(n%i==0)
-    f=f+i;
+    {
+        if(n%i==0)
+        f+=i;
+    }
     if(f>n)
     printf("True");
     else
