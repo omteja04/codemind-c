0220672141614f5dc2918f541310539aa6d222ca
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-01-29 13:07:47 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,d;     scanf("%d",&n);     int a[n],i,b[n],min,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]>0)         {             d=log10(a[i])+1;             b[i]=d;         }         else if(a[i]<0)         {             d=log10(abs(a[i]))+1;             b[i]=d;         }         else         b[i]=1;     }     min=b[0];     for(i=0;i<n;i++)     {         if(b[i]<min)         {             min=b[i];         }     }     for(i=0;i<n;i++)     {         if(b[i]==min)         c++;     }     printf("%d",c); }

---
+++ Minimum_digit_elements.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,d;
+    scanf("%d",&n);
+    int a[n],i,b[n],min,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>0)
+        {
+            d=log10(a[i])+1;
+            b[i]=d;
+        }
+        else if(a[i]<0)
+        {
+            d=log10(abs(a[i]))+1;
+            b[i]=d;
+        }
+        else
+        b[i]=1;
+    }
+    min=b[0];
+    for(i=0;i<n;i++)
+    {
+        if(b[i]<min)
+        {
+            min=b[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(b[i]==min)
+        c++;
+    }
+    printf("%d",c);
+}
